084f61f3a926f680e826bd25d5c32cce15dfb74c
TejasChitroda <chitrodatejas176@gmail.com>
2025-03-31 14:28:30 +0530

    Add project files.

---
+++ Task-2/Script/Site.js
@@ -0,0 +1,20 @@
+﻿document.getElementById("print_text").addEventListener("click", function () {
+    document.getElementById("text").innerHTML = "Hello World!";
+    document.getElementById("alertText").innerHTML = "";
+});
+
+document.getElementById("bold").addEventListener("click", function () {
+    document.getElementById("text").style.fontWeight = "bold";
+});
+
+document.getElementById("italic").addEventListener("click", function () {
+    document.getElementById("text").style.fontStyle = "italic";
+});
+
+document.getElementById("underLine").addEventListener("click", function () {
+    document.getElementById("text").style.textDecoration = "underline";
+});
+
+document.getElementById("reset").addEventListener("click", function () {
+    document.getElementById("text").style = "none";
+});
